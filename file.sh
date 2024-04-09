0e525c43fb266ee45b881ea1c4e58f0ff9028f42
Lehar1107 <agrawallehar66@gmail.com>
2024-04-09 14:48:23 +0530

    first commit with kafka

---
+++ file.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+choice="$1"
+case $choice in
+        elasticsearch)
+                echo "elastic search runnng"
+                ansible-playbook ./ansible/playbook.yml
+                ;;
+        kibana) echo "you select kibana"
+                ansible-playbook ./ansible/kibana_playbook.yml
+                ;;
+        filebeat) echo "you select filebeat"
+                ansible-playbook ./ansible/filebeat_playbook.yml
+                ;;
+        logstash) echo "you select logstash"
+                ansible-playbook ./ansible/logstash_playbook.yml
+                ;;
+        quit) echo "Quitting" exit
+                ;;
+        *) echo "default selection";;
+esac
+
+
